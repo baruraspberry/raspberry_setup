77cb0ace497a47a00d060234a388aeed49085211
baruraspberry <144846716+baruraspberry@users.noreply.github.com>
2023-09-23 13:11:57 +0100

    Update set_git_repo.bash

--- raspberry_main/raspberry_setup/set_git_repo.bash
+++ raspberry_main/raspberry_setup/set_git_repo.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/Desktop/raspberry_sensors/raspberry_setup
+cd ~/Desktop/raspberry_setup/raspberry_main/raspberry_setup
 echo "Raspberry Name"
 read raspName
 sed -i "s/raspberryname/${raspName}/" sensors.py
@@ -9,18 +9,18 @@ echo "------------------------Go to github settings and click 'add SSH key' with
 cat ~/.ssh/id_rsa.pub
 echo "-----------------------------------------Press enter after copy.--------------------------------------------"
 read pulse
-mkdir ~/Desktop/raspberry_sensors/data
+#mkdir ~/Desktop/raspberry_setup/raspberry_main/data
 cd ../data
 git init
 git config --global init.defaultBranch origin main
-git config --global user.email "kevsernuryildiz@hotmail.com.tr"
-git config --global user.name "kevsernury"
+git config --global user.email "kevsernury@gmail.com"
+git config --global user.name "baruraspberry"
 git config --global pull.rebase false
-echo git remote add origin git@github.com:kevsernury/data.git
-git remote add origin git@github.com:kevsernury/data.git
+echo git remote add origin git@github.com:baruraspberry/data.git
+git remote add origin git@github.com:baruraspberry/data.git
 sleep 3
-echo remote set-url origin git@github.com:kevsernury/data.git
-git remote set-url origin git@github.com:kevsernury/data.git
+echo remote set-url origin git@github.com:baruraspberry/data.git
+git remote set-url origin git@github.com:baruraspberry/data.git
 sleep 3
 git commit -m "first time"
 echo git branch $raspName
