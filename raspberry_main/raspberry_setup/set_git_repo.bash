f6862a3c95a07846804e30f36eb8077762013cfb
baruraspberry <144846716+baruraspberry@users.noreply.github.com>
2023-09-27 12:31:50 +0100

    Update set_git_repo.bash

--- raspberry_main/raspberry_setup/set_git_repo.bash
+++ raspberry_main/raspberry_setup/set_git_repo.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/Desktop/raspberry_setup/raspberry_main/raspberry_setup
+cd home/pi/Desktop/raspberry_setup/raspberry_main/raspberry_setup
+pwd
 echo "Raspberry Name"
 read raspName
 sed -i "s/raspberryname/${raspName}/" sensors.py
@@ -9,8 +10,8 @@ echo "------------------------Go to github settings and click 'add SSH key' with
 cat ~/.ssh/id_rsa.pub
 echo "-----------------------------------------Press enter after copy.--------------------------------------------"
 read pulse
-#mkdir ~/Desktop/raspberry_setup/raspberry_main/data
 cd ../data
+sudo chown pi:pi ~/.gitconfig
 git init
 git config --global init.defaultBranch origin main
 git config --global user.email "kevsernury@gmail.com"
@@ -22,7 +23,7 @@ sleep 3
 echo remote set-url origin git@github.com:baruraspberry/data.git
 git remote set-url origin git@github.com:baruraspberry/data.git
 sleep 3
-cd ~/Desktop/raspberry_setup/raspberry_main/data
+git init
 git add .
 git commit -m "first time"
 echo git branch $raspName
@@ -31,7 +32,5 @@ sleep 3
 echo git checkout $raspName
 git checkout $raspName
 sleep 3
-eval `ssh-agent -s`
-ssh-add ~/.ssh/*_rsa
 #git push --set-upstream origin $raspName
 #git push -u origin $raspName
