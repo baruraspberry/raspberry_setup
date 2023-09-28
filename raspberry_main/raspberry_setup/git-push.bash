e7363d95128d7b50be3346ac672b8bf5b160d132
baruraspberry <144846716+baruraspberry@users.noreply.github.com>
2023-09-28 16:40:22 +0300

    Update git-push.bash

--- raspberry_main/raspberry_setup/git-push.bash
+++ raspberry_main/raspberry_setup/git-push.bash
@@ -3,7 +3,7 @@ eval `ssh-agent -s`
 ssh-add ~/.ssh/*_rsa
 while true
 do 
- cd /home/pi/Desktop/raspberry_setup/raspberry_main/raspberry_setup
+ cd /home/pi/Desktop/raspberry_setup/raspberry_main/data
  git add .
  git commit -m "raspberryname `date`"
  git push --set-upstream origin raspberryname
